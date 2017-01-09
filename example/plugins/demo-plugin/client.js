9de692134ec7b8f824e5da6d10c72666aeaed159
Adrian Biedrzycki <a@modules.io>
2017-01-09 04:00:13 -0800

    More example plugin work

--- example/plugins/demo-plugin/client.js
+++ example/plugins/demo-plugin/client.js
@@ -1,25 +1,63 @@
 module.exports = archae => ({
   mount() {
-    let message = 'Running in the browser!';
-    let n = 0;
-    const _updateText = () => {
-      element.textContent = message + ' You clicked ' + n + ' times.';
-    };
+    const element = (() => {
+      const element = document.createElement('form');
 
-    const element = document.createElement('h1');
-    element.onclick = () => {
-      n++;
-      _updateText();
-    };
+      const textLabel = document.createElement('label');
+      textLabel.innerHTML = 'Input text: ';
+      textLabel.style.marginRight = '10px';
+      const text = document.createElement('input');
+      text.type = 'text';
+      text.placeholder = 'Enter some text';
+      text.autofocus = true;
+      textLabel.appendChild(text);
+      element.appendChild(textLabel);
+
+      const numberLabel = document.createElement('label');
+      numberLabel.innerHTML = 'Padding: ';
+      const number = document.createElement('input');
+      number.type = 'number';
+      number.value = 10;
+      numberLabel.appendChild(number);
+      element.appendChild(numberLabel);
+
+      const submit = document.createElement('input');
+      submit.type = 'submit';
+      submit.value = 'Left-pad it on the server!';
+      submit.style.display = 'block';
+      submit.style.margin = '10px 0';
+      element.appendChild(submit);
+
+      const result = document.createElement('textarea');
+      result.style.width = '400px';
+      result.style.height = '200px';
+      element.appendChild(result);
+
+      element.addEventListener('submit', e => {
+        fetch('/left-pad', {
+          method: 'POST',
+          headers: {
+            'left-pad': number.value,
+          },
+          body: text.value,
+        })
+          .then(res => res.text()
+            .then(s => {
+              result.value = s;
+            })
+          )
+          .catch(err => {
+            console.warn(err);
+          });
+
+        e.preventDefault();
+      });
+
+      return element;
+    })();
     this.element = element;
-    document.body.appendChild(element);
 
-    return {
-      changeMessage(m) {
-        message = m;
-        _updateText();
-      }
-    };
+    document.body.appendChild(element);
   },
   unmount() {
     document.body.removeChild(this.element);
--- example/plugins/demo-plugin/server.js
+++ example/plugins/demo-plugin/server.js
@@ -1,8 +1,42 @@
-module.exports = archae => ({
-  mount() {
-    console.log('Running on the server!');
-  },
-  unmount() {
-    console.log('Bye bye!');
+const leftPad = require('left-pad');
+
+module.exports = class DemoPlugin {
+  constructor(archae) {
+    this.archae = archae;
   }
-});
+
+  mount() {
+    const {archae} = this;
+    const {express, app} = archae.getCore();
+
+    function serveLeftPad(req, res, next) {
+      const n = parseInt(req.get('left-pad'), 10) || 0;
+
+      let s = '';
+      req.setEncoding('utf8');
+      req.on('data', data => {
+        s += data;
+      });
+      req.on('end', () => {
+        res.send(leftPad(s, s.length + n));
+      });
+    }
+    app.post('/left-pad', serveLeftPad);
+
+    this._cleanup = () => {
+      function removeMiddlewares(route, i, routes) {
+        if (route.handle.name === 'serveLeftPad') {
+          routes.splice(i, 1);
+        }
+        if (route.route) {
+          route.route.stack.forEach(removeMiddlewares);
+        }
+      }
+      app._router.stack.forEach(removeMiddlewares);
+    };
+  }
+
+  unmount() {
+    this._cleanup();
+  }
+}
