326aca0dea42d97452ec55574192232d2db21024
Adrian Biedrzycki <a@modules.io>
2017-08-06 13:15:31 -0700

    Bugfix cors ACAO

--- index.js
+++ index.js
@@ -584,7 +584,7 @@ class ArchaeServer extends EventEmitter {
     // cross-origin resoure sharing
     if (cors) {
       app.all('*', (req, res, next) => {
-        res.set('Access-Control-Allow-Origin', req.get('Origin'));
+        res.set('Access-Control-Allow-Origin', req.get('Host'));
         res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         res.set('Access-Control-Allow-Credentials', 'true');
