d729d2637ae2caf1023aa31890338b0c12ad503d
Adrian Biedrzycki <a@modules.io>
2017-11-12 07:27:16 -0800

    Use new WebSocket injection strategy

--- client.js
+++ client.js
@@ -1,10 +1,20 @@
+const path = require('path');
+const WebSocket = require('ws/lib/WebSocket.js');
+
 module.exports = ({protocol = 'http', host = '127.0.0.1', port = 8000} = {}) => {
-  global.location = {
+  const window = global;
+  const location = {
     protocol: protocol + ':',
     host: host + ':' + port,
     pathname: '/',
   };
-  global.WebSocket = require('ws/lib/WebSocket.js');
-  const archae = require('.');
+  const _makeArchae = require(path.join(__dirname, 'lib', 'archae'));
+  const ArchaeClient = _makeArchae({
+    window,
+    location,
+    WebSocket,
+  });
+  const archae = new ArchaeClient();
+
   return archae;
 };
